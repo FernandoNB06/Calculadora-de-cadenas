50ec1c9db968905152f2c65ec4bf46e09eef307d
FernandoNB06 <fernando.nogales@ucb.edu.bo>
2024-11-20 02:02:06 -0400

    Refactor: cambiar nombres de funciones para mejorar claridad y legibilidad

--- src/sumador.js
+++ src/sumador.js
@@ -1,17 +1,17 @@
-function calcularCadena(cadena) {
+function sumarDesdeCadena(cadena) {
   if (!cadena) return 0; // Si la cadena está vacía, devolver 0
 
-  const delimitadorRegex = generarRegexDelimitadores(cadena);
+  const delimitadorRegex = crearRegexParaDelimitadores(cadena);
 
   // Si comienza con delimitadores personalizados, separa la parte de números
   const numerosCadena = cadena.startsWith("//")
     ? cadena.slice(cadena.indexOf("\n") + 1)
     : cadena;
 
-  return dividirYSumar(numerosCadena, delimitadorRegex);
+  return sumarNumerosDesdeCadena(numerosCadena, delimitadorRegex);
 }
 
-function generarRegexDelimitadores(cadena) {
+function crearRegexParaDelimitadores(cadena) {
   const delimitadoresPorDefecto = [",", "-"];
 
   if (cadena.startsWith("//")) {
@@ -29,19 +29,19 @@ function generarRegexDelimitadores(cadena) {
   return new RegExp(delimitadoresPorDefecto.map(d => d.replace(/[.*+?^${}()|[\]\\]/g, "\\$&")).join("|"), "g");
 }
 
-function dividirYSumar(cadena, delimitadorRegex) {
+function sumarNumerosDesdeCadena(cadena, delimitadorRegex) {
   const numeros = cadena.split(delimitadorRegex).map(Number);
-  validarNumeros(numeros); // Validar números negativos
+  verificarNumerosNegativos(numeros); // Validar números negativos
   return numeros
     .filter(num => num <= 1000) // Ignorar números mayores a 1000
     .reduce((suma, num) => suma + num, 0);
 }
 
-function validarNumeros(numeros) {
+function verificarNumerosNegativos(numeros) {
   const negativos = numeros.filter(num => num < 0);
   if (negativos.length > 0) {
     throw new Error(`Números negativos no permitidos: ${negativos.join(", ")}`);
   }
 }
 
-export default calcularCadena;
+export default sumarDesdeCadena;
